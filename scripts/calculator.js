b54e2e2c986d119ab76aa8d519a9053842e068d9
lilchamp27 <oduneyebolu004@gmail.com>
2024-09-10 08:50:04 +0100

    js code for the calculator

---
+++ scripts/calculator.js
@@ -0,0 +1,20 @@
+function addition(num1, num2) {
+    let total = num1 + num2;
+    console.log(total);
+}
+
+function subtraction(num1, num2) {
+    let total = num2 - num1;
+    console.log(total);
+}
+
+
+function multiplition(num1, num2) {
+    let total = num1 * num2;
+    console.log(total);
+}
+
+function division(num1, num2) {
+    let total = num2 / num1;
+    console.log(total);
+}
